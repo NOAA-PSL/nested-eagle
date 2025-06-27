e73f29172db03d4a48a4723346c3532beb62ffdd
Timothy Andrew Smith <smith.timothy.andrew@gmail.com>
2025-06-27 09:00:58 -0700

    inference and rmse/mae computation

--- eagle/inference.py
+++ eagle/inference.py
@@ -1,18 +1,12 @@
+import os
 import sys
+import yaml
+
 from datetime import datetime
 import pandas as pd
+
 from anemoi.inference.config.run import RunConfiguration
 from anemoi.inference.runners import create_runner
-from inference_globals import (
-    CHECKPOINT,
-    LAM,
-    LAM_PATH,
-    GLOBAL_PATH,
-    LEAD_TIME,
-    OUTPUT_PATH,
-    INPUT_DATA_PATH,
-)
-
 
 def date_to_str(
     date: str,
@@ -27,19 +21,26 @@ def date_to_str(
         str -- date of initialization in str format.
     """
     dt = datetime.fromisoformat(date)
-    return dt.strftime("%Y%m%dT%H%M%SZ")
+    return dt.strftime("%Y-%m-%dT%H")
 
+def get_main_config(config_filename: str):
+    with open(config_filename, "r") as f:
+        main_config = yaml.safe_load(f)
+
+    # expand any environment variables
+    for key, val in main_config.items():
+        if "path" in key:
+            main_config[key] = os.path.expandvars(val)
+
+    # if output_path is not created, make it here
+    if not os.path.isdir(main_config["output_path"]):
+        os.makedirs(main_config["output_path"])
+
+    return main_config
 
 def create_config(
     init_date: str,
-    extract_lam: bool,
-    lead_time: int = LEAD_TIME,
-    lam: bool = LAM,
-    checkpoint: str = CHECKPOINT,
-    input_data_path: str = INPUT_DATA_PATH,
-    lam_path: str = LAM_PATH,
-    global_path: str = GLOBAL_PATH,
-    output_path: str = OUTPUT_PATH,
+    main_config: dict,
 ) -> dict:
     """
     Create the config that will be passed to anemoi-inference.
@@ -51,7 +52,7 @@ def create_config(
         extract_lam (bool): logic to extract lam domain, or run whole nested domain.
         lead_time (int): desired lead time to save out. Default=LEAD_TIME.
         lam (bool): true/false indication if LAM. Default=LAM.
-        checkpoint (str): path to checkpoint. Default=CHECKPOINT.
+        checkpoint_path (str): path to checkpoint. Default=CHECKPOINT.
         input_data_path (str): path to input data when not using LAM (e.g. you trained on 1 source). Default=INPUT_DATA_PATH.
         lam_path (str): path to regional data when using LAM. Default=LAM_PATH.
         global_path (str: path to global data when using LAM. Default=GLOBAL_PATH.
@@ -62,69 +63,68 @@ def create_config(
     """
     date_str = date_to_str(date=init_date)
 
-    if lam:
-        if extract_lam:
+    if main_config.get("lam_data_path", None) is not None:
+        if main_config.get("extract_lam", False):
             config = {
-                "checkpoint": checkpoint,
-                "date": init_date,
-                "lead_time": lead_time,
+                "checkpoint": main_config["checkpoint_path"],
+                "date": date_str,
+                "lead_time": main_config["lead_time"],
                 "input": {
                     "cutout": {
-                        "lam_0": {"dataset": lam_path},
-                        "global": {"dataset": global_path},
+                        "lam_0": {"dataset": main_config["lam_data_path"]},
+                        "global": {"dataset": main_config["global_data_path"]},
                     }
                 },
                 "output": {
                     "extract_lam": {
-                        "output": {"netcdf": {"path": f"{output_path}/lam.nc"}}
+                        "output": {"netcdf": {"path": f"{main_config['output_path']}/lam.nc"}}
                     }
                 },
             }
         else:
             config = {
-                "checkpoint": checkpoint,
-                "date": init_date,
-                "lead_time": lead_time,
+                "checkpoint": main_config["checkpoint_path"],
+                "date": date_str,
+                "lead_time": main_config["lead_time"],
                 "input": {
                     "cutout": {
-                        "lam_0": {"dataset": lam_path},
-                        "global": {"dataset": global_path},
+                        "lam_0": {"dataset": main_config["lam_data_path"]},
+                        "global": {"dataset": main_config["global_data_path"]},
                     }
                 },
-                "output": {"netcdf": f"{output_path}/{date_str}.nc"},
+                "output": {"netcdf": f"{main_config['output_path']}/{date_str}.nc"},
             }
 
     else:
         config = {
-            "checkpoint": checkpoint,
-            "date": init_date,
-            "lead_time": lead_time,
+            "checkpoint": main_config["checkpoint_path"],
+            "date": date_str,
+            "lead_time": main_config["lead_time"],
             "input": {
-                "xarray-zarr": input_data_path,
+                "dataset": main_config["input_data_path"],
             },
-            "output": {"netcdf": f"{output_path}/{date_str}.nc"},
+            "output": {"netcdf": f"{main_config['output_path']}/{date_str}.{main_config['lead_time']}h.nc"},
         }
 
     return config
 
 
-def run_inference(
+def run_forecast(
     init_date: str,
-    extract_lam: bool,
+    main_config: dict,
 ) -> None:
     """
     Inference pipeline.
 
     Args:
         init_date (str): date of initialization.
-        extract_lam (bool): true will run inference only on LAM domain.
 
     Returns:
         None -- files saved out to output path.
     """
     config_dict = create_config(
         init_date=init_date,
-        extract_lam=extract_lam,
+        main_config=main_config,
     )
     config = RunConfiguration.load(config_dict)
     runner = create_runner(config)
@@ -133,26 +133,24 @@ def run_inference(
     return
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
+def run_inference():
+    if len(sys.argv) != 2:
         print(
-            "Usage: python run_inference.py <start_date>  <end_date>  <freq>  <extract_lam>"
-        )
-        print(
-            "Example: python run_inference.py '2018-01-06T00:00:00' '2018-01-08T00:00:00' '12h' 'False'"
+            "Usage: python run_inference.py recipe.yaml"
         )
         sys.exit(1)
 
-    start_date = sys.argv[1]
-    end_date = sys.argv[2]
-    freq = sys.argv[3]
-    extract_lam = sys.argv[4]
+    config_filename = sys.argv[1]
+    main_config = get_main_config(config_filename)
 
-    dates = pd.date_range(start=start_date, end=end_date, freq=freq)
+    dates = pd.date_range(start=main_config["start_date"], end=main_config["end_date"], freq=main_config["freq"])
+
+    print(f"Starting inference with initial condition dates:\n{dates}")
 
     nc_files = []
     for d in dates:
-        run_inference(
+        run_forecast(
             init_date=str(d),
-            extract_lam=extract_lam,
+            main_config=main_config,
         )
+        print(f"Done with {d}")
